0f810b73b32327560032c2bb54da87a31f84348c
Hiruna Harankahadeniya <hiruna.19@cse.mrt.ac.lk>
2024-05-04 20:20:13 +0530

    Notifications added for folder mount

--- src/renderer/src/components/FolderMount.jsx
+++ src/renderer/src/components/FolderMount.jsx
@@ -3,9 +3,12 @@ import { Button, useMediaQuery, IconButton } from '@mui/material';
 import CreateNewFolderIcon from '@mui/icons-material/CreateNewFolder';
 import useApi from '../services/hooks/useApi';
 import axios from 'axios';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { setToast } from '../store/reducers/toast';
 
 const FileMountComponent = () => {
+  const dispatch = useDispatch();
+
   async function openFolder() {
     const folderPath = await window.electronAPI.openFolder();
     // console.log(folderPath);
@@ -14,10 +17,30 @@ const FileMountComponent = () => {
     // for now, just copy the contents of the folder to the data folder in backend
     if (folderPath === undefined) {
       console.log('No folder selected');
+      dispatch(
+        setToast({
+          toast: { open: true, message: 'No folder selected', severity: 'error' },
+        })
+      );
       return;
     }
-    const res = await axios.post(`${import.meta.env.VITE_PERSONAL_CLOUD_URL}/mount_album`, {folderPath})
-    console.log(res.data);
+    await axios.post(`${import.meta.env.VITE_PERSONAL_CLOUD_URL}/mount_album`, {folderPath})
+    .then((res) => {
+      console.log(res.data);
+      dispatch(
+        setToast({
+          toast: { open: true, message: 'Folder mounted. Processing images... This may take a while.', severity: 'info' },
+        })
+      );
+    }
+    ).catch((error) => {
+      console.log(error);
+      dispatch(
+        setToast({
+          toast: { open: true, message: 'Error while mounting folder', severity: 'error' },
+        })
+      );
+    });
   }
 
   const matchesXs = useMediaQuery((theme) => theme.breakpoints.down('lg'));
