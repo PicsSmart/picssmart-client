b6582ec8b5b0552834f675a29833bcc889f678b5
Hiruna Harankahadeniya <hiruna.19@cse.mrt.ac.lk>
2024-05-13 19:42:50 +0530

    Re-fetch albums and faces when a folder is mounted

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -9,6 +9,10 @@ import { changeMountingStatus } from './store/reducers/mountingStatus';
 import { setMedia } from './store/reducers/media';
 import { getMediaApi } from './services/apiService/media';
 import { ERROR_MESSAGES } from './utils/constants';
+import { getAlbumsApi } from './services/apiService/albums';
+import { setAlbums } from './store/reducers/albums';
+import { getFacesApi } from './services/apiService/people';
+import { setFaces } from './store/reducers/faces';
 
 // ==============================|| APP - THEME, ROUTER, LOCAL  ||============================== //
 
@@ -30,6 +34,40 @@ const App = () => {
     }
   };
 
+  const getAlbums = async () => {
+    try {
+      const { data } = await getAlbumsApi();
+      dispatch(
+        setAlbums({
+          albums: data
+        })
+      );
+    } catch (exception) {
+      dispatch(
+        setToast({
+          toast: { open: true, message: ERROR_MESSAGES.ALBUMS, severity: 'error' }
+        })
+      );
+    }
+  }
+
+  const getFaces = async () => {
+    try {
+      const { data } = await getFacesApi();
+      dispatch(
+        setFaces({
+          faces: data
+        })
+      );
+    } catch (exception) {
+      dispatch(
+        setToast({
+          toast: { open: true, message: ERROR_MESSAGES.FACES, severity: 'error' }
+        })
+      );
+    }
+  }
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -43,6 +81,8 @@ const App = () => {
       dispatch(changeMountingStatus(false));
       // console.log(message);
       getMedia();
+      getAlbums();
+      getFaces();
     });
   }
   , [])
